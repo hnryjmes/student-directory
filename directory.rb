e952d81035dab9b50788fa35a360d99370abdff0
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-05 16:53:17 +0100

    interactive menu

--- directory.rb
+++ directory.rb
@@ -1,3 +1,29 @@
+def interactive_menu
+students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
+
 def input_students
   puts "Please enter the names, ages and cohorts of the students like this: name, age, cohort. To default to november cohort, you can leave it blank."
   puts "To finish, just hit return twice"
@@ -68,12 +94,4 @@ def print_footer(students)
   puts string
 end
 
-students = input_students
-while students.size == 0
-  puts "ERROR: List empty, please add a student"
-  students = input_students
-end
-#nothing happens until we call the methods
-print_header
-print(students)
-print_footer(students)
+interactive_menu
