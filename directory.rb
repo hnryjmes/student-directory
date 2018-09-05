b71d08f5311c8b485c0c0f418446228f47ab9852
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-05 14:19:58 +0100

    exercise 2

--- directory.rb
+++ directory.rb
@@ -23,7 +23,7 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
+  students.select { |student| student[:name][0] == "d"}.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
