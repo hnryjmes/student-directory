105f3b591c44f6cbb7a29e2b1b6e269bb93f92cc
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-05 16:03:54 +0100

    basic sorting by cohort

--- directory.rb
+++ directory.rb
@@ -1,8 +1,13 @@
 def input_students
   puts "Please enter the names, ages and cohorts of the students like this: name, age, cohort. To default to november cohort, you can leave it blank."
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
+  # pre-populated data
+  students = [
+    {name: "Dr. Hannibal Lecter", age: 50, cohort: :november},
+    {name: "Darth Vader", age: 38, cohort: :november},
+    {name: "Nurse Ratched", age: 75, cohort: :september
+    },
+  ]
   # get the first name
   input = gets.chomp
   # while the name is not empty, repeat this code
@@ -14,7 +19,8 @@ def input_students
     end
     name, age, cohort = arr[0], arr[1], arr[2].to_s
     # add the student hash to the array
-    students << {name: name, age: age, cohort: cohort }
+    students << {name:
+    name, age: age, cohort: cohort }
     puts "Now we have #{students.count} students"
     # get another name from the user
     input = gets.chomp
@@ -29,13 +35,30 @@ def print_header
 end
 
 def print(students)
+  # get students by cohort
+  sorted = {}
+  students.each do |student|
+    if sorted[student[:cohort]] == nil
+      sorted[student[:cohort]] = [].push(student)
+    else
+      sorted[student[:cohort]].push(student)
+    end
+  end
+  sorted.each do |cohort, sorted_students|
+    puts "#{cohort} cohort: "
+    puts " "
+    sorted_students.each do |student|
+      puts "* #{student[:name]}, #{student[:age]}"
+    end
+    puts "-------------"
+  end
+
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]}, age #{student[:age]} (#{student[:cohort]} cohort)"
+    # puts "#{index + 1}. #{student[:name]}, age #{student[:age]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
-  puts "-------------"
   string = "Overall, we have #{students.count} great students"
   puts string
 end
