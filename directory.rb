702aad44eb73bc9b65f73779b877e7bdb360c925
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-05 14:21:41 +0100

    back to normal

--- directory.rb
+++ directory.rb
@@ -23,7 +23,7 @@ def print_header
 end
 
 def print(students)
-  students.select { |student| student[:name].length < 12}.each_with_index do |student, index|
+  students.each_with_index do |student, index|
     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
