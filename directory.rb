162cd63eae727b98faa8ed02f0d987bb8ffefc46
Henry Cooksley <henry.cooksley@gmail.com>
2018-09-06 12:36:56 +0100

    auto try for students.csv

--- directory.rb
+++ directory.rb
@@ -94,6 +94,11 @@ def load_students(filename = "students.csv")
 end
 
 def try_load_students
+  filename = "students.csv"
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  end
   filename = ARGV.first# first argument from the command line
   return if filename.nil? # get out of the method if it isn't given
   if File.exists?(filename) # if it exists
